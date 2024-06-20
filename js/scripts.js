57e4bce5b0be9e2aea7066d419c39d81f5713718
luckys3000 <jwillard@396willard.com>
2024-06-19 20:27:51 -0500

    feat: add script to build pokedex
    Add initial script to build Pokedex as an example application

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Create a new variable called pokemonList and assign to it a blank array.
+let pokemonList = [];
+
+//Add several objects to the array and at least 3 key-value pairs for each object.
+pokemonList = [
+	{ name: 'Charizard', height: '5\' 07"', type: ['fire', 'flying'] },
+	{ name: 'Blastoise', height: '5\' 03"', type: ['water'] },
+	{ name: 'Beedrill', height: '3\' 03"', type: ['bug', 'poison'] },
+	{ name: 'Ninetales', height: '3\' 07"', type: ['fire'] },
+	{ name: 'Vileplume', height: '3\' 11"', type: ['grass', 'poison'] },
+];
