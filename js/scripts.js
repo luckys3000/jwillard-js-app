ef768e776b6df5d2b6829b570db868f56a3eb981
luckys3000 <jwillard@396willard.com>
2024-07-02 19:39:41 -0500

    refactor: wrap pokemonList in IIFE and update forEach loop
     - Wrap pokemonList in IIFE
     - Create public functions 'add', 'getAll'
     - Create check for add function
        allowing only objects
     - Update forEach loop with public
       functions 'getAll'

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,38 @@
-//Create a new variable called pokemonList and assign to it a blank array.
-let pokemonList = [];
+//create pokemonRepository variable to hold what IIFE will return
+let pokemonRepository = (function () {
+	let pokemonList = [];
+	pokemonList = [
+		{ name: 'Charizard', height: '5\' 07"', type: ['fire', 'flying'] },
+		{ name: 'Blastoise', height: '5\' 03"', type: ['water'] },
+		{ name: 'Beedrill', height: '3\' 03"', type: ['bug', 'poison'] },
+		{ name: 'Ninetales', height: '3\' 07"', type: ['fire'] },
+		{ name: 'Vileplume', height: '3\' 11"', type: ['grass', 'poison'] },
+	];
 
-//Add several objects to the array and at least 3 key-value pairs for each object.
-pokemonList = [
-	{ name: 'Charizard', height: '5\' 07"', type: ['fire', 'flying'] },
-	{ name: 'Blastoise', height: '5\' 03"', type: ['water'] },
-	{ name: 'Beedrill', height: '3\' 03"', type: ['bug', 'poison'] },
-	{ name: 'Ninetales', height: '3\' 07"', type: ['fire'] },
-	{ name: 'Vileplume', height: '3\' 11"', type: ['grass', 'poison'] },
-];
+	//Function to add a new pokemon to the pokemonList array
+	function add(pokemon) {
+		//Check if the parameter is an object
+		if (typeof pokemon === 'object' && pokemon !== null) {
+			pokemonList.push(pokemon);
+		} else {
+			document.write('Error: Only objects can be added to the pokemonList');
+		}
+	}
 
-// Iterate over each element in the pokemonList array, assigning each element to the variable 'pokemon'
-pokemonList.forEach(function (pokemon) {
+	//Function to return all pokemon in the pokemonList array
+	function getAll() {
+		return pokemonList;
+	}
+
+	//Return an object with reference to the add and getAll functions
+	return {
+		add: add,
+		getAll: getAll,
+	};
+})();
+
+// Iterate over each pokemon in the repository
+pokemonRepository.getAll().forEach(function (pokemon) {
 	//Create a variable heightInInches which converts a height string in the format "feet'inches "" to inches
 	let heightInInches = parseInt(pokemon.height.split("' ")[0]) * 12 + parseInt(pokemon.height.split("' ")[1].replace('"', ''));
 
