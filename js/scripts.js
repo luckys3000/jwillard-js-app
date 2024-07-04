dae34a4e155f7f3fe720e153493ca71a109a3b8e
luckys3000 <jwillard@396willard.com>
2024-07-04 13:32:09 -0500

    refactor(js): Restructure and optimize javascript
     - Remove variable converting height and inches to inches
     - Remove conditional expression the prints string to tallest Pokémon
     - Rename variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,29 @@
 //create pokemonRepository variable to hold what IIFE will return
 let pokemonRepository = (function () {
-	let pokemonList = [];
-	pokemonList = [
-		{ name: 'Charizard', height: '5\' 07"', type: ['fire', 'flying'] },
-		{ name: 'Blastoise', height: '5\' 03"', type: ['water'] },
-		{ name: 'Beedrill', height: '3\' 03"', type: ['bug', 'poison'] },
-		{ name: 'Ninetales', height: '3\' 07"', type: ['fire'] },
-		{ name: 'Vileplume', height: '3\' 11"', type: ['grass', 'poison'] },
+	let repository = [];
+	repository = [
+		{ name: 'Charizard', height: '5\' 07"', types: ['Fire', 'Flying'] },
+		{ name: 'Blastoise', height: '5\' 03"', types: ['Water'] },
+		{ name: 'Beedrill', height: '3\' 03"', types: ['Bug', 'Poison'] },
+		{ name: 'Ninetales', height: '3\' 07"', types: ['Fire'] },
+		{ name: 'Vileplume', height: '3\' 11"', types: ['Grass', 'Poison'] },
 	];
 
 	//Function to add a new pokemon to the pokemonList array
 	function add(pokemon) {
 		//Check if the parameter is an object
-		if (typeof pokemon === 'object' && pokemon !== null) {
-			pokemonList.push(pokemon);
+		if (typeof pokemon === 'object' && pokemon !== null && 'name' in pokemon && 'height' in pokemon && 'types' in pokemon) {
+			repository.push(pokemon);
 		} else {
-			document.write('Error: Only objects can be added to the pokemonList');
+			document.write('Error: Pokemon is not correct');
 		}
 	}
 
 	//Function to return all pokemon in the pokemonList array
 	function getAll() {
-		return pokemonList;
+		return repository;
 	}
 
-	//Return an object with reference to the add and getAll functions
 	// Function to log pokemon details to console
 	function showDetails(pokemon) {
 		console.log(`Name: ${pokemon.name}, Height: ${pokemon.height}, Types:     
@@ -56,15 +55,10 @@ let pokemonRepository = (function () {
 })();
 
 // Iterate over each pokemon in the repository
-pokemonRepository.getAll().forEach(function (pokemon) {
-	//Create a variable heightInInches which converts a height string in the format "feet'inches "" to inches
-	let heightInInches = parseInt(pokemon.height.split("' ")[0]) * 12 + parseInt(pokemon.height.split("' ")[1].replace('"', ''));
+pokemonRepository.add({ name: 'Pikachu', height: 0.3, types: ['Electric'] });
 
-	//Create conditional expression that will print the name and height of all Pokemon and adds text string to the tallest Pokemon
-	if (heightInInches > 63 && heightInInches < 75) {
-		document.write(pokemon.name + ' (Height: ' + pokemon.height + ')', ' - Wow, that’s big! ' + '<br>');
-	} else {
-		document.write(pokemon.name + ' (Height: ' + pokemon.height + ')' + '<br>');
-	}
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
 	pokemonRepository.addListItem(pokemon);
 });
